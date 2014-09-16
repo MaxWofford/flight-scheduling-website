43dac5953310fa6bb50665395a675e3580cf0bee
Max Wofford <max.speed.wofford@gmail.com>
2014-09-16 17:35:08 -0600

    Added new static pages and redirect to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'teams' => 'pages#team'
+  get 'plan' => 'pages#schedule'
+  #get 'team/:id' => 'pages#team', as: 'patient'
 
   devise_for :users
+  devise_scope :user do
+    get '/sign-in' => "devise/sessions#new", :as => :login
+  end
 end
