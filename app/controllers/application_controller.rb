9875e97f3efe2ebd2e539638f8ee6067fc055afa
Max Wofford <max.speed.wofford@gmail.com>
2014-09-16 19:15:37 -0600

    Added footer and render timer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :set_start_time
+
+  def set_start_time
+    @start_time = Time.now.usec
+  end
+
   protected
   def authenticate_user!
     if user_signed_in?
