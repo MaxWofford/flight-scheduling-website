93c26c41121a41b467347201aa06081a3b44bd96
Max Wofford <max.speed.wofford@gmail.com>
2014-09-18 19:04:39 -0600

    Added user creation to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+title = ["Mr", "Mrs", "Ms", "Miss", "Dr", "Sr", "Jr"]
+names = ["Smith", "Jackson", "Bob", "Siri", "Rob", "John", "Angela", "Maria", "Casey"]
+
+user = User.new(
+    :email                 => "#{title[rand(title.length - 1)]}.#{names[rand(names.length - 1)]}@example.com",
+    :password              => "12345678",
+    :password_confirmation => "12345678"
+)
+user.save
